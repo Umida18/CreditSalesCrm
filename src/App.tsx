b2277f70611e10567d13751e3e79ecea6b4e50bd
Umida18 <akbarovau42@gmail.com>
2025-02-18 14:42:22 +0500

    .

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import LoginPage from "./pages/login";
 import Home from "./pages/home/home";
 import Zone from "./components/zone";
@@ -7,13 +7,15 @@ import CollectorMoney from "./components/collector/collectorsTable";
 import Workplace from "./components/workplace/workplace";
 
 function App() {
-  // const token = localStorage.getItem("token");
+  const token = localStorage.getItem("token");
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
-
-        <Route path="/login" element={<LoginPage />} />
+        <Route path="/" element={token ? <Home /> : <Navigate to="/login" />} />
+        <Route
+          path="/login"
+          element={token ? <Navigate to="/" /> : <LoginPage />}
+        />
 
         <Route path="/collector" element={<Collector />} />
         <Route path="/collectorMoney" element={<CollectorMoney />} />
