598a7e646ea5c4194d84644003f3bcd4f2c61179
Umida18 <akbarovau42@gmail.com>
2025-02-18 11:05:08 +0500

    .

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LoginPage from "./pages/login";
 import Home from "./pages/home/home";
 import Zone from "./components/zone";
@@ -11,11 +11,9 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={token ? <Home /> : <Navigate to="/login" />} />
-        <Route
-          path="/login"
-          element={token ? <Navigate to="/" /> : <LoginPage />}
-        />
+        <Route path="/" element={<Home />} />
+
+        <Route path="/login" element={<LoginPage />} />
 
         <Route path="/collector" element={<Collector />} />
         <Route path="/collectorMoney" element={<CollectorMoney />} />
