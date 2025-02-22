e5534a06cf6edac3690440f176b9c8f2ba24ffaa
Umida18 <akbarovau42@gmail.com>
2025-02-22 17:07:20 +0500

    corrected

--- src/App.tsx
+++ src/App.tsx
@@ -10,25 +10,28 @@ import UsersPage from "./components/dashboard/userPage";
 import DashboardCollector from "./components/dashboardCollector/dashboardCollector";
 import UsersCollec from "./components/dashboardCollector/usersCallector";
 import StatisticsContent from "./components/dashboardCollector/statistic";
+import ErrorBoundary from "./components/ErrorBoundary";
 
 function App() {
   return (
-    <BrowserRouter>
-      <ToastContainer />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<LoginPage />} />
-        {/* <Route path="/collector" element={<Collector />} /> */}
-        <Route path="/collector" element={<Collector />} />
-        <Route path="/collectorLogin" element={<CollectorLoginPage />} />
-        <Route path="/zone" element={<Zone />} />
-        <Route path="/collectorDashboard" element={<DashboardCollector />} />
-        <Route path="/workplace" element={<Workplace />} />
-        <Route path="/statistic" element={<StatisticsContent />} />
-        <Route path="/users/:id" element={<UsersPage />} />
-        <Route path="/usersCollector/:id" element={<UsersCollec />} />
-      </Routes>
-    </BrowserRouter>
+    <ErrorBoundary>
+      <BrowserRouter>
+        <ToastContainer />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<LoginPage />} />
+          {/* <Route path="/collector" element={<Collector />} /> */}
+          <Route path="/collector" element={<Collector />} />
+          <Route path="/collectorLogin" element={<CollectorLoginPage />} />
+          <Route path="/zone" element={<Zone />} />
+          <Route path="/collectorDashboard" element={<DashboardCollector />} />
+          <Route path="/workplace" element={<Workplace />} />
+          <Route path="/statistic" element={<StatisticsContent />} />
+          <Route path="/users/:id" element={<UsersPage />} />
+          <Route path="/usersCollector/:id" element={<UsersCollec />} />
+        </Routes>
+      </BrowserRouter>
+    </ErrorBoundary>
   );
 }
 
